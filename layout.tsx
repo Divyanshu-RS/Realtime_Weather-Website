a6a0acb36f63cb79ef19e80586a87eecf4302c0f
Divyanshu-RS <divyanshuranjan.9999@gmail.com>
2024-07-06 17:17:36 +0530

    new vesel insight layout.tsx

---
+++ layout.tsx
@@ -0,0 +1,19 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
+ 
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body>
+        {children}
+        <SpeedInsights />
+      </body>
+    </html>
+  );
+}
